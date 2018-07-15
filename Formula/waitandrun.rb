8fc86a6e5c6f008c81c5b96e2d3d8f395927c083
thecontinium <thecontinium@outlook.com>
2018-07-15 15:33:50 +0800

    Fix

--- Formula/waitandrun.rb
+++ Formula/waitandrun.rb
@@ -1,7 +1,7 @@
-class WaitAndRun< Formula
+class Waitandrun< Formula
   homepage 'https://github.com/thecontinium/homebrew-waitandrun/'
   url 'https://github.com/thecontinium/homebrew-waitandrun.git'
-  version '0.0.3'
+  version '0.0.4'
 
   skip_clean 'bin'
 
